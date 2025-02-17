83d88e5d39a02fbebdf4baded2aa5fc4f1d8e008
diegoleam <diegoconstrucoespb@gmail.com>
2025-02-17 19:57:44 -0300

    EXERCICIO 13 FOR

---
+++ src/exercicios/Exercicio13.java
@@ -0,0 +1,16 @@
+package exercicios;
+
+// IMPRIMIR NA TELA DE 1 ATE 100 COMANDO FOR
+
+public class Exercicio13 {
+
+    public static void main (String [] args){
+
+        for (int contador = 1; contador <= 100;contador++){
+
+            System.out.println(contador);
+        }
+
+    }
+
+}

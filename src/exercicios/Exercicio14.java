1daac95eee9bf42ba5d661c3e73775ae11e5ad52
diegoleam <diegoconstrucoespb@gmail.com>
2025-02-17 20:00:53 -0300

    EXERCICIO 14 FOR

---
+++ src/exercicios/Exercicio14.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+// IMPRIMIR NA TELA DE 1 ATE 100 DE F0RMA DECRESCENTE FOR
+
+public class Exercicio14 {
+
+    public static void main (String [] args){
+
+        for (int contador = 100; contador >= 1;contador--){
+
+            System.out.println(contador);
+        }
+
+    }
+
+}
+
+

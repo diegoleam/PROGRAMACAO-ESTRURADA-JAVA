187e8a55f6d66fa3c7a10e892b326a8851dbf1b4
diegoleam <diegoconstrucoespb@gmail.com>
2025-02-17 18:38:40 -0300

    QUESTÃO DE PROVA

---
+++ src/exercicios/QuestaoProva4.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+// IMPRIMIR NA TELA DE 1 ATE 300 DE 3 EM 3, FORMA DECRESCENTE COM O WHILE
+
+public class QuestaoProva4 {
+    public static void main (String []args){
+
+        int contador = 300;
+
+        while (contador >=1){
+
+            System.out.println(contador);
+            contador -= 3;
+
+        }
+
+    }
+}

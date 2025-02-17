1d1767a451d1215d4158798dad42720da49799b6
diegoleam <diegoconstrucoespb@gmail.com>
2025-02-17 18:08:10 -0300

    QUESTÃO DE PROVA

---
+++ src/exercicios/QuestaoProva1.java
@@ -0,0 +1,25 @@
+package exercicios;
+
+// 7. Declare 3 variáveis de tipos diferentes e atribua valores mostre na tela (1,5 pontos).
+
+public class QuestaoProva1 {
+
+    public static void main (String [] args){
+
+        // VARIAVEIS
+
+        String nome = "DIEGO";
+        int idade = 33;
+        double altura = 1.71;
+
+        // EXIBI
+
+        System.out.println(" NOME : " +nome);
+        System.out.println(" IDADE : " +idade);
+        System.out.println(" ALTURA : " +altura);
+
+
+    }
+
+
+}

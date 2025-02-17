8852e1ef49d22c3caf7a1c4b66628ff2d3dc933e
diegoleam <diegoconstrucoespb@gmail.com>
2025-02-17 15:29:11 -0300

    EXERCICIO 8

---
+++ src/exercicios/Exercicio8.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+// CONTADOR COM WHILE DE 1 ATE 100 DE F0RMA DECRESCENTE
+
+public class Exercicio8 {
+
+ public static void main (String [] args){
+
+     // variavel do contador começa de 100 descrecente
+     int contador = 100;
+
+     while (contador >=1){
+
+         System.out.println(contador);
+
+         // incrementa de forma decrescente
+         contador -= 1;
+
+     }
+ }
+
+}

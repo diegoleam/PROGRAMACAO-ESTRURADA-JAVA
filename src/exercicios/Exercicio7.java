f9938749de9b3f7e4d5d4043ce59f38e44e95cd3
diegoleam <diegoconstrucoespb@gmail.com>
2025-02-17 15:18:09 -0300

    EXERCICIO 7

---
+++ src/exercicios/Exercicio7.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+// CONTADOR COM WHILE 1 ATE 100 0RDEM CRESSCENTE
+
+public class Exercicio7 {
+
+    public static void main (String [] args ){
+
+
+        // variavel que começa o contador
+        int contador =1;
+
+        while (contador <= 100){
+
+            System.out.println(contador);
+
+            // incrementa 1
+            contador += 1;
+
+        }
+
+    }
+
+}
